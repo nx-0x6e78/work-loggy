f33b4b672753280a0d1c3c28597b698e2bcf53c3
nx_0x6e78 <95649209+nx-0x6e78@users.noreply.github.com>
2025-07-09 23:18:16 +0200

    .

--- src/components/home/homePage.tsx
+++ src/components/home/homePage.tsx
@@ -2,14 +2,11 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 
 export default async function HomePage() {
-    const session = await auth.api.getSession({
-        headers: await headers(),
-    })
+	const session = await auth.api.getSession({
+		headers: await headers(),
+	});
 	return (
-		<section className="grid grid-cols-2">
-			<article>
-				<h1>Good morning {session?.user.name} {new Date().toTimeString()}</h1>
-			</article>
+		<section className="grid grid-cols-2 gap-4">
 		</section>
 	);
 }
